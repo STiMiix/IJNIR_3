5a43d5ed223a724748565d834011e055f192dad5
Иван <piramida1006@mail.ru>
2025-06-03 16:36:51 +0500

    Исправил

--- IJNIR_3/math_3d.h
+++ IJNIR_3/math_3d.h
@@ -4,8 +4,31 @@
 #include <stdio.h>
 #include <math.h>
 
-#define ToRadian(x) ((x) * 3.14 / 180.0f)
-#define ToDegree(x) ((x) * 180.0f / 3.14)
+#define ToRadian(x) ((x) * 3.14f / 180.0f)
+#define ToDegree(x) ((x) * 180.0f / 3.14f)
+
+struct Vector2i
+{
+    int x;
+    int y;
+};
+
+struct Vector2f
+{
+    float x;
+    float y;
+
+    Vector2f()
+    {
+    }
+
+    Vector2f(float _x, float _y)
+    {
+        x = _x;
+        y = _y;
+    }
+};
+
 
 struct Vector3f
 {
@@ -24,15 +47,73 @@ struct Vector3f
         z = _z;
     }
 
+    Vector3f& operator+=(const Vector3f& r)
+    {
+        x += r.x;
+        y += r.y;
+        z += r.z;
+
+        return *this;
+    }
+
+    Vector3f& operator-=(const Vector3f& r)
+    {
+        x -= r.x;
+        y -= r.y;
+        z -= r.z;
+
+        return *this;
+    }
+
+    Vector3f& operator*=(float f)
+    {
+        x *= f;
+        y *= f;
+        z *= f;
+
+        return *this;
+    }
+
     Vector3f Cross(const Vector3f& v) const;
 
     Vector3f& Normalize();
+
+    void Rotate(float Angle, const Vector3f& Axis);
 
     void Print() const
     {
         printf("(%.02f, %.02f, %.02f", x, y, z);
     }
 };
+
+
+inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
+{
+    Vector3f Ret(l.x + r.x,
+        l.y + r.y,
+        l.z + r.z);
+
+    return Ret;
+}
+
+inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
+{
+    Vector3f Ret(l.x - r.x,
+        l.y - r.y,
+        l.z - r.z);
+
+    return Ret;
+}
+
+inline Vector3f operator*(const Vector3f& l, float f)
+{
+    Vector3f Ret(l.x * f,
+        l.y * f,
+        l.z * f);
+
+    return Ret;
+}
+
 
 class Matrix4f
 {
@@ -75,6 +156,21 @@ public:
     void InitPersProjTransform(float FOV, float Width, float Height, float zNear, float zFar);
 };
 
+
+struct Quaternion
+{
+    float x, y, z, w;
+
+    Quaternion(float _x, float _y, float _z, float _w);
+
+    void Normalize();
+
+    Quaternion Conjugate();
+};
+
+Quaternion operator*(const Quaternion& l, const Quaternion& r);
+
+Quaternion operator*(const Quaternion& q, const Vector3f& v);
 
 #endif	/* MATH_3D_H */
 #pragma once
